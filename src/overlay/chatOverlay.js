6d773022533693437036e71fba0a910138974790
Luciano Tassara <lucianotassara@gmail.com>
2020-10-28 02:08:01 -0300

    deploy

--- src/overlay/chatOverlay.js
+++ src/overlay/chatOverlay.js
@@ -101,7 +101,7 @@ let orar = false;
 let allowed = [];
 let blacklistUsersLluvia = [];
 let blacklistUsersOracion = [];
-const socket = io(SOCKET_URL);
+const socket = io(SOCKET_URL, {transport: websocket});
 
 function manageMessage(element){
   // Guardo el mensaje en la variable comment
